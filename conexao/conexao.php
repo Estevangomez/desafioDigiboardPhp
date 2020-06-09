817e6bde53396197c85865af3d6a0c7a891e1fcc
Estevangomez <gomes.estevan1@gmail.com>
2020-06-08 20:38:38 -0400

    Ajustes de nome de usuario da conexao

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -8,7 +8,7 @@ class Conexao
 	private static $instance;
 	public static function getConn(){
 		if(!isset(self::$instance)):
-			self::$instance = new PDO('mysql:host=127.0.0.1;dbname=digiboard;','root','ESTE15wr#');
+			self::$instance = new PDO('mysql:host=127.0.0.1;dbname=digiboard;','digiboard','');
 		endif;
 
 			return self::$instance;
